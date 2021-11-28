13d695676e6496163a9b0cdca3790e2d32175823
Boatshoes24 <wmoulton4@gmail.com>
2021-11-27 20:10:00 -0500

    added active class for current page to keep link highlighted

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import {
   FiInfo,
   FiTwitch,
@@ -10,19 +10,33 @@ import {
 } from 'react-icons/fi';
 
 const Links = () => {
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split('/');
+
   return (
     <div className='navbar'>
       <ul>
         <li>
-          <div id='link1' className='link-container'>
-            <Link to='/'>
+          <div
+            id='link1'
+            className={`${
+              splitLocation[1] === '' ? 'active-link1' : ''
+            } link-container`}
+          >
+            <Link exact to='/'>
               <FiHome />
               <span>Home</span>
             </Link>
           </div>
         </li>
         <li>
-          <div id='link2' className='link-container'>
+          <div
+            id='link2'
+            className={`${
+              splitLocation[1] === 'about' ? 'active-link2' : ''
+            } link-container`}
+          >
             <Link to='/about'>
               <FiInfo />
               <span>About</span>
@@ -30,7 +44,12 @@ const Links = () => {
           </div>
         </li>
         <li>
-          <div id='link3' className='link-container'>
+          <div
+            id='link3'
+            className={`${
+              splitLocation[1] === 'gallery' ? 'active-link3' : ''
+            } link-container`}
+          >
             <Link to='/gallery'>
               <FiImage />
               <span>Gallery</span>
@@ -38,7 +57,12 @@ const Links = () => {
           </div>
         </li>
         <li>
-          <div id='link4' className='link-container'>
+          <div
+            id='link4'
+            className={`${
+              splitLocation[1] === 'streams' ? 'active-link4' : ''
+            } link-container`}
+          >
             <Link to='/streams'>
               <FiTwitch />
               <span>Streams</span>
@@ -46,7 +70,12 @@ const Links = () => {
           </div>
         </li>
         <li>
-          <div id='link5' className='link-container'>
+          <div
+            id='link5'
+            className={`${
+              splitLocation[1] === 'logs' ? 'active-link5' : ''
+            } link-container`}
+          >
             <a
               href='https://www.warcraftlogs.com/guild/id/367786'
               rel='noopener noreferrer'
@@ -58,7 +87,12 @@ const Links = () => {
           </div>
         </li>
         <li>
-          <div id='link6' className='link-container'>
+          <div
+            id='link6'
+            className={`${
+              splitLocation[1] === 'apply' ? 'active-link6' : ''
+            } link-container`}
+          >
             <Link to='/apply'>
               <FiEdit />
               <span>Apply</span>
