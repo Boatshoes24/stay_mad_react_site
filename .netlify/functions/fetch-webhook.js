15141f7ce1b5bc16b046a701abcec009a541281a
Boatshoes24 <wmoulton4@gmail.com>
2023-03-29 23:45:00 -0400

    bc

--- .netlify/functions/fetch-webhook.js
+++ .netlify/functions/fetch-webhook.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const handler = async (event) => {
     const POST_URL = process.env.REACT_APP_WEBHOOK_URL;
 
+    console.log(event);
+
     try {
         const { data } = axios.post(POST_URL)
 
